473c29ca040d360c7658e83634c30613eea22f88
benggala12 <110433196+benggala12@users.noreply.github.com>
2022-12-05 19:13:03 +0700

    Add files via upload

---
+++ DDG-akmal-XIR2-main/DDG-akmal-XIR2-main/Percabangan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Percabangan : MonoBehaviour {
+    int nilai = 88;
+    void Start(){
+        if (nilai > 90){
+            Debug.Log("Nilai A");
+        }else if (nilai > 70 && nilai <= 90) {
+            Debug.Log("Nilai B");
+        }else if (nilai > 50 && nilai <= 70) {
+            Debug.Log("Nilai C");
+        }else{
+            Debug.Log("Nilai D");
+        }
+    }
+}
---
+++ DDG-akmal-XIR2-main/DDG-akmal-XIR2-main/PercabanganSwitchCase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PercabanganSwitchCase : MonoBehaviour
+{
+    int angka = 2;
+    void Start() {
+        switch (angka)
+        {
+            case 1:
+                Debug.Log("Telor");
+            break;
+            case 2:
+                Debug.Log("Ayam Bakar");
+            break;
+            case 3:
+                Debug.Log("Sate Ayam");
+            break;
+            default:
+                Debug.Log("Jus Apel");
+            break;
+        }
+    }
+}
